e9acc3a98a38719c0ac94e07caee99344e0de508
Robert Boily <RobertSBoily@gmail.com>
2018-05-29 18:01:14 -0400

    Comments

--- spec/pages/chapter1_page.rb
+++ spec/pages/chapter1_page.rb
@@ -23,20 +23,24 @@ class Chapter1Page
 	end
 
 	# Could maybe simplify these a bit my using an instance variable for menu.
+	# These functions are similar to attr_accessor, but we want additional behaviours (e.g. setter changes the page).
 	def dropdown=(value)
 		# Public: Must select value by its text value, e.g. "Selenium IDE"
 		menu = driver.find_element(SELENIUM_DROPDOWN)
 		menu.select_by(:value, value)
+		self
 	end
 
 	def dropdown
 		# Public: Returns the current value set in the dropdown.
 		menu = driver.find_element(SELENIUM_DROPDOWN)
 		menu.selected_options
+		self
 	end
 
 	def get_dropdown_options
 		# Public: Returns the text of all available values in the dropdown.
+		# Does NOT return a page object - should I fix this? IE return [page object, texts] ?
 		menu = driver.find_element(SELENIUM_DROPDOWN)
 		options = menu.find_elements(:tag_name => "option")
 		texts = []
